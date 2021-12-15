7dd49aa38dfcf263a66b1f1e9286ad0b5c42ca06
Cristian Espinosa <cespinosa6@ucmerced.edu>
2021-12-14 17:50:34 -0800

    data structure for model fixed, need to work on in and out channels

--- lib/CNN.py
+++ lib/CNN.py
@@ -3,38 +3,47 @@ import torch
 class CNN(torch.nn.Module):
     def __init__(self):
         super.__init__()
+        '''
+        Input: 
+        First convolution: 
+        First MaxPool:
+        Second 
+        '''
         self.layer_list = torch.nn.ModuleList([
-            # 8 channels in, 12 channels out, 3x3 kernel
-            # 8x26x26 -> 12x24x24
+            # 8 channels in, 12 channels out, 5x5 kernel
+            # 1x28x28 -> 32x24x24
             # (26 - 3 + 0 + 0)/1 + 1 ref formula for input dim
             torch.nn.Conv2d(1,32,5),
             # linear activation function
             torch.nn.ReLU(inplace=True),
             # max-pooling operator
-            # 12x24x24 -> 32x12x12
-            # 24/2
+            # 32x24x24 -> 32x12x12
+            # ((24 - 2)/2) + 1 = 12
             torch.nn.MaxPool2d(2, 2),
             # 12 channels in, 32 channels out, 5x5 kernel
-            # 32x12x12 -> 32x8x8
-            # (12 - 5 + 0 + 0)/1 + 1 ref formula for input dim
+            # 32x12x12 -> 64x8x8
+            # (12 - 5 + 2*0)/1 + 1 = 8 ref formula for input dim
             torch.nn.Conv2d(32,64,5),
             # linear activation function
             torch.nn.ReLU(inplace=True),
-            # 32x8x8 -> 32x4x4 = 512
-            # 8/2
+            # 64x8x8 -> 64x4x4 = 1024
+            # (8 - 2)/2 + 1 = 4
             torch.nn.MaxPool2d(2, 2),
         ])
         
-        # Classifier: FIX ME -> Modify hyperparameters accordingly!!!!
         self.classifier = torch.nn.Sequential(
-            # Fully-Connected, 512 elements in, 2 elements out
-            torch.nn.Linear(512, 256),
+            # Fully-Connected, 1024 elements in, 10 elements out
+            torch.nn.Linear(1024, 256),
             torch.nn.ReLU(inplace=True),
             torch.nn.Linear(256, 160),
             torch.nn.ReLU(inplace=True),
             torch.nn.Linear(160, 67),
             torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(67, 2),
-            torch.nn.Sigmoid(),
+            torch.nn.Linear(67, 10),
+            torch.nn.Softmax(),
         )
-    pass
+        
+        
+        def forward(self, x):
+            pass
+        
