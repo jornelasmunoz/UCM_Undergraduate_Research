4d20e0de437c8213cce784edeb15c24266c72cc6
Jocelyn Ornelas <jornelasmunoz@ucmerced.edu>
2023-01-25 14:24:11 -0800

    Added deep autoencoder to pyfile. Not needed now, but maybe for future reference

--- jornelasmunoz/lib/CNN.py
+++ jornelasmunoz/lib/CNN.py
@@ -76,3 +76,33 @@ class CNN(nn.Module):
     
     
 
+# Creating a DeepAutoencoder class
+class DeepAutoencoder(torch.nn.Module):
+    def __init__(self, img_size):
+        super().__init__()  
+        self.img_size = img_size
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(self.img_size * self.img_size, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 10)
+        )
+          
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(10, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, self.img_size * self.img_size),
+            torch.nn.Sigmoid()
+        )
+  
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
