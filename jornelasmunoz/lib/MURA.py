11ba2d909bbe47ee4d12447df9a01efaad9ea344
Jocelyn Ornelas Munoz <jornelasmunoz@ucmerced.edu>
2024-08-08 09:58:36 -0700

    doing experiments with torch conv with circular padding

--- jornelasmunoz/lib/MURA.py
+++ jornelasmunoz/lib/MURA.py
@@ -90,8 +90,8 @@ def FFT_convolve(A, B, p=None):
     fft_B = fft2(B)
     conv_AB = torch.real(ifft2(torch.multiply(fft_A,fft_B)))
     # conv_AB = torch.roll(conv_AB, [int((p-1)/2),int((p-1)/2)], dims=(0,1))
-    conv_AB = torch.roll(conv_AB, [int((p+1)/2),int((p+1)/2)], dims=(0,1))
-    # conv_AB = torch.roll(conv_AB, [int((p)/2),int((p)/2)], dims=(0,1))
+    # conv_AB = torch.roll(conv_AB, [int((p+1)/2),int((p+1)/2)], dims=(0,1))
+    conv_AB = torch.roll(conv_AB, [int((p)/2),int((p)/2)], dims=(0,1))
     return conv_AB
 
 def add_Gaussian_noise(og_image, desired_snr):
